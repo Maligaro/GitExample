9004c5c05d28c9e1479312e84ab20596634bdd0a
Skopin Nikita <skopin@skrin.ru>
2024-04-21 21:21:51 +0500

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <stdio>
+
+int Square(int x){
+    //TODO
+}
+
+void Print(int x){
+    //TODO
+}
+
+int main(){
+    int count;
+    cin >> count;
+    for (int i = 0; i < count; i++)
+    {
+        int s = Square(i);
+        Print(i);
+    }
+    return 0;
+}
